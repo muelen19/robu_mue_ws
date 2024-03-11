f398dfeb6f8284f3cbe2994fff013022461bf77e
muelen19 <116274896+muelen19@users.noreply.github.com>
2024-03-11 11:04:56 +0100

    11.03,2024

--- src/robu/launch/ex13_action_launch.py
+++ src/robu/launch/ex13_action_launch.py
@@ -1,28 +1,37 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        )
-    ])
+    # Setze die ROS_DOMAIN_ID Systemumgebungsvariable auf deine Katalognummer
+    ros_domain_id = os.environ.get('ROS_DOMAIN_ID', '19')
+    os.environ['ROS_DOMAIN_ID'] = ros_domain_id
 
+    return LaunchDescription([
+        # Starte den fibonacci_server Node
+        Node(
+            package='robu',
+            executable='fibonacci_server',
+            name='fibonacci_action_server'
+        ),
+
+        # Starte den fibonacci_client Node
+        Node(
+            package='robu',
+            executable='fibonacci_client',
+            name='fibonacci_action_client'
+        ),
+
+        # Warte 5 Sekunden
+        ExecuteProcess(
+            cmd=['sleep', '5'],
+            output='screen',
+            on_exit=[ # Ausführung der CLI-Anweisung zum Aufruf der Action fibonacci mit dem Goal order=5
+                ExecuteProcess(
+                cmd=['ros2', 'action', 'send_goal', 'fibonacci', 'robu_interfaces/action/Fibonacci', 'order:=5'],
+                output='screen'
+                )]
+        ),
+       
+    ])
